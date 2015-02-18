035914de49500431e0ad9eb7e7043591a97fc30b
zgreenhalge <zgreenhalge@gmail.com>
2015-02-18 12:33:20 -0500

    Finished exif
    EXIF can now properly display the IFD entry data

--- exif.py
+++ exif.py
@@ -112,7 +112,8 @@ TAGS={      0x100:  "ImageWidth",
 typeLength=[0,1,1,2,4,8,1,1,2,4,8,4,8]
 
 class IFDEntry:
-	printStr = "{:>05X} {:20s} {!r}"
+	"""A container class for all data relevant to an IFD entry"""
+	printStr = "{:>5s} {:20s} {}"
 	def __init__(self, fd, offsetStart):
 		self.location   = fd.tell()
 		self.tag        = unpack(">H", fd.read(2))[0]
@@ -123,41 +124,39 @@ class IFDEntry:
 		if self.dataLength > 4:
 			self.offset = unpack(">L", fd.read(4))[0]
 			fd.seek(offsetStart+self.offset)
-			print("Reading from", hex(offsetStart+self.offset))
-			self.data   = unpack(">{:}B".format(self.dataLength), fd.read(self.dataLength))[0]
+			self.data   = fd.read(self.dataLength)
 		else:
 			self.offset = 0
-			self.data   = unpack(">L", fd.read(4))[0]
-		self.data = dataFromBytes(self.format, self.components, bytes(self.data))
-		print(self.format, self.components, self.dataLength, hex(self.offset))
-		print(len(self.data), type(self.data))
-		print(str(self.data).strip())
+			self.data   = fd.read(self.dataLength)
+		self.data       = dataFromBytes(self.format, self.components, bytes(self.data))
 
 		fd.seek(self.location+12)
-		print(self.printStr.format(self.tag, self.name, self.data))
+		print(self.printStr.format("{:x}".format(self.tag), self.name, self.data))
 
 class IFD:
+	"""Basically just a list of IFDEntry"""
 	def __init__(self, fd, offset):
 		self.entrynum = unpack(">H", fd.read(2))[0]
 		self.entries = []
 		print("Number of IFD Entries:", self.entrynum)
-		while len(self.entries) <= self.entrynum:
+		while len(self.entries) < self.entrynum:
 			self.entries.append(IFDEntry(fd, offset))
 
 
 
 class App:
+	"""A container class for all data relevant to an App"""
 	printStr = "[0x{:04X}] Marker 0x{:04X} size=0x{:04X}"
 	def __init__(self, marker, fd):
-		if marker[0] == 0xFFDA:
-			print("Image data found")
+		"""Extract the data for the app from the marker and file descriptor"""
+		if marker == b'\xFF\xDA': #image data header
 			self.marker = unpack(">H", marker)[0]
 			self.size = unpack(">H", fd.read(2))[0]
 			self.location = fd.tell() - 2
 			self.next = self.size + self.location + 2
 			print(self.printStr.format(self.location, self.marker, self.size))
-			return
-		if marker[0] != 0xFF:
+			sys.exit(0)
+		if marker[0] != 0xFF: #All headers are two bytes, starting with 0xFF
 			print("Invalid marker found.", marker, "Exiting.")
 			sys.exit(1)
 		self.marker   = unpack(">H", marker)[0]
@@ -165,6 +164,13 @@ class App:
 		self.location = fd.tell() - 4
 		self.next     = self.size + self.location + 2
 		self.name     = unpack(">6s", fd.read(6))[0].strip(b'\x00')
+		print(self.printStr.format(self.location, self.marker, self.size))
+		if checkforExif():
+			self.IFD = IFD(fd, self.location+10)
+		fd.seek(self.next)
+
+	def checkforExif():
+		"""Returns true if this app is the Exif data. Can exit the program if an error is detected"""
 		if self.name == b'Exif':
 			temp = fd.read(2)
 			if temp != b'MM':
@@ -176,20 +182,42 @@ class App:
 				sys.exit(1)
 			self.offset = unpack(">L", fd.read(4))[0]
 			if self.offset != fd.tell() - self.location - 10: #location - start - 10 bytes to first M
-				print("Incorect IFD offset in marker", self.name, "Exiting.")
+				print("Incorect IFD offset in marker", self.name)
+				print("Was", fd.tell() - self.location - 10, "expected", self.offset)
 				sys.exit(1)
-			print("Offset to IFD:", self.offset)
-			print("Offset reference byte:", hex(self.location+10))
-			self.IFD = IFD(fd, self.location+10)
-			#print EXIF data here
-		fd.seek(self.next)
-		print(self.printStr.format(self.location, self.marker, self.size))
+			return True
+		return False
 
+def dataFromBytes(fmt, components, bytestr):
+	"""Takes a bytestr and returns it in the specified format"""
+	ret = bytestr
+	if   fmt == 1: #byte
+		ret = unpack(">{:}B".format(len(bytestr)), bytestr)[0]
+	elif fmt == 2: #ASCII string
+		ret = bytes.decode(bytestr)
+	elif fmt == 3: #short
+		ret = unpack(">{:}H".format(components), bytestr)[0]
+	elif fmt == 4: #long
+		ret = unpack(">{:}L".format(components), bytestr)[0]
+	elif fmt == 5: #rational
+		ret = unpackRational(components, bytestr)
+	elif fmt == 7: #raw data
+		ret = "".join(*unpack(">{:}B".format(components), bytestr))
+	return ret
 
-	def print_all():
-		pass
+def unpackRational(components, bytestr):
+	"""Unpacks a given number of rationals from the bytestr"""
+	ret = []
+	size = len(ret)
+	while size <= components:
+		string = bytestr[size:size+8]
+		if len(string) >= 8:
+			ret.append("{}/{}".format(*unpack(">LL", string)))
+		size += 8
+	return ret
 
 def main(args):
+	"""Checks if the file passed is a JPEG, and reads in all JPEG apps if it is"""
 	if len(args) != 1:
 		usage()
 		sys.exit(2)
@@ -199,35 +227,9 @@ def main(args):
 			print("File passed is not a JPEG")
 			sys.exit(1)
 		marker = fd.read(2)
-		while marker != 0xFFD9: #0xFFD9 == EOF in JPEG
+		while marker != b'\xFF\xD9': #0xFFD9 == EOF in JPEG
 			apps.append(App(marker, fd))
 			marker = fd.read(2)
-
-def dataFromBytes(fmt, components, bytestr):
-	ret = bytestr
-	if   fmt == 1:
-		ret = unpack(">{:}B".format(len(bytestr)), bytestr)[0]
-	elif fmt == 2:
-		ret = bytes.decode(bytestr)
-	elif fmt == 3: 
-		ret = unpack(">{:}H".format(components), bytestr)[0]
-	elif fmt == 4:
-		ret = unpack(">{:}L".format(components), bytestr)[0]
-	elif fmt == 5:
-		ret = unpackRational(components, bytestr)
-	elif fmt == 7:
-		ret = "".join(*unpack(">{:}B".format(components), bytestr))
-	return ret
-
-def unpackRational(components, bytestr):
-	ret = []
-	size = len(ret)
-	while size <= components:
-		string = bytestr[size:size+8]
-		if len(string) >= 8:
-			ret.append("{:s}{:s}".format(*unpack(">LL", string)))
-		size += 8
-	return ret
 
 def usage():
 	print("exif <file>")
